155fa86a25797fb184dbab2ff594f199dfa0272a
Harsha <harsha.vemu@cognizant.com>
2018-12-02 21:34:31 +0100

    array list update

--- src/mobile/computing/ws1819/server/MessageResource.java
+++ src/mobile/computing/ws1819/server/MessageResource.java
@@ -1,6 +1,8 @@
 package mobile.computing.ws1819.server;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -21,7 +23,9 @@ import mobile.computing.ws1819.Message;
 
 @Path("harsha")
 public class MessageResource {
-
+    Object messageArray[] = new Object[3];
+    ArrayList<Object> messageList = new ArrayList<Object>();
+    
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getMessage() throws JsonProcessingException {
@@ -53,19 +57,28 @@ public class MessageResource {
 		return "OK";
 	}
 
-	@POST
+	@POST	
 	@Produces(MediaType.TEXT_PLAIN)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public String createMessage(String messageAsJSONstring)
 			throws JsonParseException, JsonMappingException, IOException {
-		System.out.println("\nReceived POST Request with JSON String:\n" + messageAsJSONstring);
-
+		//ArrayList<Message > list=new ArrayList<>(); 
+    
 		// Deserialise JSON message
-		ObjectMapper mapper = new ObjectMapper();
-		Message message = mapper.readValue(messageAsJSONstring, Message.class);
-		System.out.println("Creating Message Object...\n" + message);
+		//ObjectMapper mapper = new ObjectMapper();
+		//Message message = mapper.readValue(messageAsJSONstring, Message.class);
+		
+		
+		messageList.add(messageAsJSONstring);
 
+			//messageArray = messageList.toArray(messageArray);
+		
+		System.out.println("Creating Message Object...\n" + messageList.size() + messageList);
+
+		
 		return "OK";
+		
+		
 	}
 
 	@DELETE
